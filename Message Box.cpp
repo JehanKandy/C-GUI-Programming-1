92fbfcce1faf60886ff7820350e8abad95b2dbe3
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-11-24 06:43:54 -0500

    Add files via upload

---
+++ Message Box.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevinst, LPSTR args, int ncmdshow)
+{
+    MessageBox(NULL,"JehanKandy My First GUI Message box","JK-JEHAN KANDY",MB_YESNO);
+    return 0;
+}
